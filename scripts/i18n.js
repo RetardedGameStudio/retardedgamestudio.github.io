34e3a05d3b2cbf340060e69b033da147ebc0baf2
Corentin M <corentin.montel@2brik.com>
2025-02-14 16:55:41 +0100

    I18n

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -1,24 +1,38 @@
 class I18n {
-    locale = 'en-US'
     translations = {};
+    lang = 'fr';
+
     constructor(translations) {
         this.translations = translations;
     }
 
     init() {
-        this.locale = this.getLang()
-        console.log(this.locale)
+        this.lang = this.getLang()
+        console.log(this.getLang())
     }
 
     getLang() {
+        if (localStorage.getItem("lang") !== null) {
+            return localStorage.getItem("lang")
+        }
+
         if (navigator.languages !== undefined)
             return navigator.languages[0].split('-')[0];
         return navigator.language.split('-')[0];
     }
 
+    setLang(language) {
+        localStorage.setItem("lang", language)
+        this.lang = language;
+        this.translatePage();
+    }
+
     translate(key)
     {
-        return this.translations[this.locale][key]
+        if (this.translations[this.lang] === undefined) {
+            return
+        }
+        return this.translations[this.lang][key]
     }
 
 
